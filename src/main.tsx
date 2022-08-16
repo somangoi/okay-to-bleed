f3c4ec40ce418286687cdd09ba36e501874ebb6c
ette9844 <ette9844@gmail.com>
2022-08-16 10:39:24 +0900

    move routes to pages folder

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
-import Period from "./routes/period";
-import Contact from "./routes/contact";
-import Support from "./routes/support";
+import Period from "./pages/period";
+import Contact from "./pages/contact";
+import Support from "./pages/support";
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
