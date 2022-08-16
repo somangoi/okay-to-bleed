0149f9b6fe9c12bce4c3ed6ed4b205033a605d81
ette9844 <ette9844@gmail.com>
2022-08-16 15:58:00 +0900

    hotfix

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import App from './App';
-import Period from './pages/period';
-import Contact from './pages/contact';
-import Support from './pages/support';
+import Period from './pages/Period';
+import Contact from './pages/Contact';
+import Support from './pages/Support';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
