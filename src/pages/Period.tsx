c72152384414c264d99e59f4c750f42b111ecb84
ette9844 <ette9844@gmail.com>
2022-08-16 10:40:05 +0900

    add lottie anim

--- src/pages/Period.tsx
+++ src/pages/Period.tsx
@@ -1,7 +1,10 @@
+import GrowUpAnim from "../components/GrowUpAnim";
+
 export default function Period() {
   return (
     <main style={{ padding: "1rem 0" }}>
       <h2>About Period</h2>
+      <GrowUpAnim />
     </main>
   );
 }
