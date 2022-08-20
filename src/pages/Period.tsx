ea114edab23d4027d156a3bea98cea0092a66971
somangoi <somi.c.hwang@gmail.com>
2022-08-21 01:24:47 +0900

    add anim files to period file

--- src/pages/Period.tsx
+++ src/pages/Period.tsx
@@ -2,6 +2,9 @@ import Anim from '../components/AnimationFrame';
 import animationData1_1 from '../anim/1_1.json';
 import animationData1_2 from '../anim/1_2.json';
 import animationData2_1 from '../anim/2_1.json';
+import animationData2_2 from '../anim/2-2.json';
+import animationData3_1 from '../anim/3-1.json';
+import animationData3_2 from '../anim/3-2.json';
 import Subtitle from '../components/Subtitle';
 
 export default function Period() {
@@ -11,6 +14,9 @@ export default function Period() {
       <Anim animData={animationData1_1} virtualHeight="400vh" />
       <Anim animData={animationData1_2} virtualHeight="400vh" />
       <Anim animData={animationData2_1} virtualHeight="700vh" />
+      <Anim animData={animationData2_2} virtualHeight="700vh" />
+      <Anim animData={animationData3_1} virtualHeight="700vh" />
+      <Anim animData={animationData3_2} virtualHeight="700vh" />
       <Subtitle />
     </main>
   );
