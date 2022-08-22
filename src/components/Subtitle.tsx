ffc6d3e0d6a008a8cf9e9bd57314f708b353406f
Jay-JYKim91 <jay.jykim91@gmail.com>
2022-08-22 22:25:44 +0900

    test: subtitle using scroll event

--- src/components/Subtitle.tsx
+++ src/components/Subtitle.tsx
@@ -1,11 +1,41 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import subtitlesData from '../subtitle/subtitles.json';
+
+const getSubtitle = () => {
+  const result = subtitlesData.find(data => {
+    return data.start <= window.pageYOffset && data.end >= window.pageYOffset;
+  });
+
+  return result || { text: '' };
+};
 
 function Subtitle() {
+  const [currentSubtitle, setCurrentSubtitle] = useState('');
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const { text } = getSubtitle();
+      if (currentSubtitle !== text) {
+        setCurrentSubtitle(text);
+      }
+    };
+
+    window.addEventListener('scroll', () => {
+      const offsetY = window.pageYOffset || document.documentElement.scrollTop;
+      if (offsetY % 20 === 0) {
+        // console.log('window.scrollY', window.pageYOffset);
+        handleScroll();
+      }
+    });
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <SubtitleBox>
-      <p>Subtitle Subtitle Subtitle Subtitle Subtitle</p>
-    </SubtitleBox>
+    <SubtitleBox>{currentSubtitle && <Sub>{currentSubtitle}</Sub>}</SubtitleBox>
   );
 }
 
@@ -16,6 +46,11 @@ const SubtitleBox = styled.div`
   display: flex;
   justify-content: center;
   width: 100%;
+`;
+
+const Sub = styled.p`
+  background: rgba(0, 0, 0, 0.5);
+  padding: 0.5vh 2vh;
   text-align: center;
 `;
 
