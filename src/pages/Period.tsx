a7d6963773d96330f62528f99cff537b507bb40b
somangoi <somi.c.hwang@gmail.com>
2022-09-03 17:13:31 +0900

    update route of lottie json files

--- src/pages/Period.tsx
+++ src/pages/Period.tsx
@@ -8,38 +8,38 @@ function Period() {
     <main style={{ padding: '1rem 0' }}>
       <Animation
         id="lottie-1-1"
-        animationSrc="../anim/1_1.json"
+        animationSrc="/src/anim/1_1.json"
         virtualHeight="600vh"
         maxFrame={300}
         stopOffset={0.1}
       />
       <Animation
         id="lottie-1-2"
-        animationSrc="../anim/1_2.json"
+        animationSrc="/src/anim/1_2.json"
         virtualHeight="300vh"
         maxFrame={150}
       />
       <Animation
         id="lottie-2-1"
-        animationSrc="../anim/2_1.json"
+        animationSrc="/src/anim/2_1.json"
         virtualHeight="600vh"
         maxFrame={300}
       />
       <Animation
         id="lottie-2-2"
-        animationSrc="../anim/2_2.json"
+        animationSrc="/src/anim/2_2.json"
         virtualHeight="240vh"
         maxFrame={120}
       />
       <Animation
         id="lottie-3-1"
-        animationSrc="../anim/3_1.json"
+        animationSrc="/src/anim/3_1.json"
         virtualHeight="300vh"
         maxFrame={150}
       />
       <Animation
         id="lottie-3-2"
-        animationSrc="../anim/3_2.json"
+        animationSrc="/src/anim/3_2.json"
         virtualHeight="300vh"
         maxFrame={150}
       />
