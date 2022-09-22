9cd0d9ad01a2ab5fb2592617d288ccf0866951bd
somangoi <somi.c.hwang@gmail.com>
2022-09-23 00:19:01 +0900

    add route for png page

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import './index.css';
+import NewPeriod from './pages/PngSequencePeriod';
 
 const TRACKING_ID = import.meta.env.VITE_REACT_APP_GOOGLE_ANALYTICS_TRACKING_ID;
 
@@ -21,6 +22,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
         <Routes>
           <Route path="/" element={<App />}>
             <Route path="/" element={<Period />} />
+            <Route path="/png" element={<NewPeriod />} />
             <Route path="contact" element={<Contact />} />
             <Route path="support" element={<Support />} />
             <Route
