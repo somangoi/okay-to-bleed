592dd5415c011e20e604a641a4d60b8fc6d1cd92
somangoi <somi.c.hwang@gmail.com>
2022-09-25 23:29:49 +0900

    add png sequence

---
+++ src/hooks/useImage.tsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from 'react';
+
+interface stateProps {
+  image: HTMLImageElement | undefined;
+  status: string;
+}
+
+const defaultState: stateProps = { image: undefined, status: 'loading' };
+export const useImage = (url: string, crossOrigin?: string) => {
+  const res = useState(defaultState);
+  const image = res[0].image;
+  const status = res[0].status;
+
+  const setState = res[1];
+
+  useEffect(() => {
+    if (!url) return;
+    const img = document.createElement('img');
+
+    function onload() {
+      setState({ image: img, status: 'loaded' });
+    }
+
+    function onerror() {
+      setState({ image: undefined, status: 'failed' });
+    }
+
+    img.addEventListener('load', onload);
+    img.addEventListener('error', onerror);
+    crossOrigin && (img.crossOrigin = crossOrigin);
+    img.src = url;
+
+    return () => {
+      img.removeEventListener('load', onload);
+      img.removeEventListener('error', onerror);
+      setState(defaultState);
+    };
+  }, [url, crossOrigin]);
+  return [image, status];
+};
