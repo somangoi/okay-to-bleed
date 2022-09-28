c475e436ab605184fad183978ac6d3999fd2cdfc
Hyejin Jeon <ette9844@gmail.com>
2022-09-29 02:06:19 +0900

    add title, scroll guide

--- src/config/i18n/ko/index.ts
+++ src/config/i18n/ko/index.ts
@@ -1,5 +1,7 @@
 import PeriodFAQ from './PeriodFAQ.json';
+import Common from './Common.json';
 
 export default {
+  Common,
   PeriodFAQ,
 };
--- src/pages/Period.tsx
+++ src/pages/Period.tsx
@@ -1,11 +1,25 @@
+// @ts-nocheck
 import React from 'react';
+import styled from 'styled-components';
 import Animation from '../components/animation/AnimationFrame';
 import Subtitle from '../components/subtitle/Subtitle';
 import PeriodFAQ from './PeriodFAQ';
+import '@lottiefiles/lottie-player';
+import { useTranslation } from 'react-i18next';
 
 function Period() {
+  const { t } = useTranslation('Common');
   return (
     <main style={{ padding: '1rem 0' }}>
+      <Title>{t('title')}</Title>
+      <lottie-player
+        src="https://assets7.lottiefiles.com/packages/lf20_ADqq0Z.json"
+        background="transparent"
+        speed="1"
+        style={{ width: 150, height: 150, margin: 'auto' }}
+        loop
+        autoplay
+      ></lottie-player>
       <Animation
         id="lottie-1-1"
         animationSrc="/anim/1_1.json"
@@ -48,5 +62,14 @@ function Period() {
     </main>
   );
 }
+
+const Title = styled.div`
+  padding: auto;
+  font-size: 80px;
+  width: 100%;
+  text-align: center;
+  margin: 5rem 0px;
+  line-height: 1.5;
+`;
 
 export default React.memo(Period);
