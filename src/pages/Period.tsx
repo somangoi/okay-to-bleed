c75ac8cf6e036295efd5bc00ed003a66991162db
ette9844 <ette9844@gmail.com>
2022-10-14 17:47:27 +0900

    add Title component

--- src/pages/Period.tsx
+++ src/pages/Period.tsx
@@ -1,26 +1,17 @@
-// @ts-nocheck
 import React from 'react';
 import styled from 'styled-components';
 import Animation from '../components/animation/AnimationFrame';
 import SubtitleBox from '../components/subtitle/SubtitleBox';
+import Title from '../components/Title';
 import PeriodFAQ from './PeriodFAQ';
 import Ch1SubtitlesData from '../config/i18n/en/Ch1Subtitles.json';
-import '@lottiefiles/lottie-player';
 import { useTranslation } from 'react-i18next';
 
 function Period() {
   const { t } = useTranslation('Common');
   return (
     <main style={{ padding: '1rem 0' }}>
-      <Title>{t('title')}</Title>
-      <lottie-player
-        src="https://assets7.lottiefiles.com/packages/lf20_ADqq0Z.json"
-        background="transparent"
-        speed="1"
-        style={{ width: 150, height: 150, margin: 'auto' }}
-        loop
-        autoplay
-      ></lottie-player>
+      <Title text={t('title')} />
       <Animation
         id="lottie-1-1"
         animationSrc="/anim/1_1.json"
@@ -66,14 +57,5 @@ function Period() {
     </main>
   );
 }
-
-const Title = styled.div`
-  padding: auto;
-  font-size: 80px;
-  width: 100%;
-  text-align: center;
-  margin: 5rem 0px;
-  line-height: 1.5;
-`;
 
 export default React.memo(Period);
