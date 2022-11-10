0f6112a0c532d2dd9ebbbffdd0384c73bc1a512d
ette9844 <ette9844@gmail.com>
2022-11-10 11:40:01 +0900

    adjust width of ToggleList

--- src/pages/PeriodFAQ.tsx
+++ src/pages/PeriodFAQ.tsx
@@ -10,13 +10,14 @@ type Props = {
 };
 
 const PeriodFAQ = (props: Props) => {
-  const { t } = useTranslation(['PeriodFAQ', 'Period']);
+  const { t } = useTranslation('PeriodFAQ');
   return (
     <PeriodFAQContainer
       virtualHeight={props.virtualHeight}
       currentScene={props.currentScene}
     >
-      <ComponentTitle>{t('Chapter2-3Title', { ns: 'Period' })}</ComponentTitle>
+      <ComponentTitle>{t('PeriodFaqTitle')}</ComponentTitle>
+      <ComponentSmallTitle>{t('PeriodFaqSmallTitle')}</ComponentSmallTitle>
       <ToggleList title={t('1-1')} content={t('1-2')} />
       <ToggleList title={t('2-1')} content={t('2-2')} />
       <ToggleList title={t('3-1')} content={t('3-2')} />
@@ -42,12 +43,25 @@ const PeriodFAQContainer = styled.div<{
   width: 100%;
 `;
 
-const ComponentTitle = styled.h1`
-  font-size: 2rem;
-  margin-bottom: 3rem;
+const ComponentTitle = styled.h2`
+  font-size: 1.7rem;
+  line-height: 1.8rem;
+  color: #fff;
+  margin-bottom: 0;
 
   @media (max-width: 600px) {
-    font-size: 1.6rem;
+    font-size: 1.3rem;
+    line-height: 1.5rem;
   }
 `;
+
+const ComponentSmallTitle = styled.p`
+  margin-bottom: 4rem;
+  color: rgba(255, 255, 255, 0.7);
+
+  @media (max-width: 600px) {
+    margin-bottom: 2rem;
+  }
+`;
+
 export default PeriodFAQ;
